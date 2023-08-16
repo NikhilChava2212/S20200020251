7fa751981714b1d2e68c88e912ef4a939ac92329
NikhilChava2212 <venkatasainikhil.c20@iiits.in>
2023-08-16 19:43:36 +0530

    Train app

--- server/server.js
+++ server/server.js
@@ -1,39 +1,39 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = 4000;
+const port = 5005;
+
+app.listen(port,()=>{console.log(`listening to port ${port}`)});
+
 
 app.get('/numbers', async (req, res) => {
-    const urls = req.query.url || [];
-    const timeout = 1000;
+    const maxTime = 5000;
+    const allUrls = req.query.url || [];
+    var answer = [];
 
-    const fetchPromises = urls.map(url => {
-        return axios.get(url, { timeout })
-            .then(response => response.data.numbers || [])
-            .catch(error => {
-                console.error(`Error fetching data from ${url}:`, error.message);
-                return [];
-            });
+    for (const url of allUrls) { 
+        try {
+            const response = await axios.get(url, { timeout: maxTime }); 
+
+            if (response.data.numbers.length > 0) { 
+                answer.push(...response.data.numbers); 
+            }
+        } catch (error) {
+            console.error(`Error fetching data from ${url}:`, error.message);
+        }
+    }
+
+    console.log(allUrls);
+    console.log(answer);
+
+    answer = answer.sort((a, b) => a - b);
+
+    
+    answer = answer.filter((value, index, self) => {
+        return self.indexOf(value) === index;
     });
 
-    try {
-        const results = await Promise.allSettled(fetchPromises);
-
-        const validResponses = results
-            .filter(result => result.status === 'fulfilled')
-            .map(result => result.value)
-            .flat();
-
-        const uniqueNumbers = [...new Set(validResponses)];
-        const sortedNumbers = uniqueNumbers.sort((a, b) => a - b); // Sort in increasing order
-
-        res.json({ numbers: sortedNumbers });
-    } catch (error) {
-        console.error('An error occurred:', error.message);
-        res.status(500).json({ error: 'An error occurred while processing the requests.' });
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(answer);
+    const obj={"numbers":answer};
+    res.json(obj); 
 });
